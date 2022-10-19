23690d0dc24a968c10ab6a0b18f3f48cd4644377
domkoz <kozlowski_d@hotmail.com>
2022-10-19 22:58:51 +0200

    Finished

--- main.py
+++ main.py
@@ -2,26 +2,35 @@ import numpy as np
 
 
 class PCA:
-    def __init__(self, number_components):
-        self.number_components = number_components
-        self.mean = None
+    def __init__(self, n_components):
+        self.n_components = n_components
         self.components = None
+        self.mean = None
 
+    def fit(self, X):
+        # Mean centering
+        self.mean = np.mean(X, axis=0)
+        X = X - self.mean
 
-    def fit(self, data):
-        self.mean = np.mean(data, axis=0)
-        data = data - self.mean
-        covariance = np.cov(data.T)
-        eigenvalues, eigenvectors = np.linalg.eig(covariance)
-        idxs = np.argsort(eigenvectors.T)[::-1]
+        # covariance, function needs samples as columns
+        cov = np.cov(X.T)
+
+        # eigenvalues, eigenvectors
+        eigenvalues, eigenvectors = np.linalg.eig(cov)
+
+        # -> eigenvector v = [:,i] column vector, transpose for easier calculations
+        # sort eigenvectors
+        eigenvectors = eigenvectors.T
+        idxs = np.argsort(eigenvalues)[::-1]
         eigenvalues = eigenvalues[idxs]
         eigenvectors = eigenvectors[idxs]
 
+        # store first n eigenvectors
+        self.components = eigenvectors[0 : self.n_components]
 
-        #store n
-        self.components = eigenvectors[0:self.number_components]
-    def transform(self, data):
-        data = data - self.mean
-        return np.dot(data, self.components.T)
+    def transform(self, X):
+        # project data
+        X = X - self.mean
+        return np.dot(X, self.components.T)
 
 
--- pca_tests.py
+++ pca_tests.py
@@ -1,12 +1,35 @@
-from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
-import numpy as np
+from sklearn.datasets import load_iris
+
 from main import PCA
 
-data = load_iris()
-data_X = data.data
-data_Y = data.target
+# Testing
+if __name__ == "__main__":
+    # Imports
+    import matplotlib.pyplot as plt
+    from sklearn import datasets
 
-pca = PCA(2)
-pca.fit(data_X)
-X_projected = pca.transform(data_X)
+    # data = datasets.load_digits()
+    data = datasets.load_iris()
+    X = data.data
+    y = data.target
+
+    # Project the data onto the 2 primary principal components
+    pca = PCA(2)
+    pca.fit(X)
+    X_projected = pca.transform(X)
+
+    print("Shape of X:", X.shape)
+    print("Shape of transformed X:", X_projected.shape)
+
+    x1 = X_projected[:, 0]
+    x2 = X_projected[:, 1]
+
+    plt.scatter(
+        x1, x2, c=y, edgecolor="none", alpha=0.8, cmap=plt.cm.get_cmap("viridis", 3)
+    )
+
+    plt.xlabel("Principal Component 1")
+    plt.ylabel("Principal Component 2")
+    plt.colorbar()
+    plt.show()
